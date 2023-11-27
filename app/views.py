8a5138bfda5f9b5c5f5186daf692145baf8e9958
Govardhan369 <govardhanreddy88t@gmail.com>
2023-11-27 17:40:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+def srujana(request):
+    return render(request,'srujana.html')
+
+def venky(request):
+    return render(request,'venky.html')
+
+# Create your views here.
